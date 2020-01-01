5af243aad14e8b592c8eb07e49a774feef5c8a9e
lionel <lionelk@163.com>
2020-01-01 13:20:22 +0800

    Initial commit

---
+++ src/main/java/com/shicheng/dutyinfo/DutyinfoApplication.java
@@ -0,0 +1,13 @@
+package com.shicheng.dutyinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutyinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutyinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shicheng/dutyinfo/DutyinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shicheng.dutyinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutyinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
